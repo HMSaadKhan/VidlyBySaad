108b4a0bc3660dfc312f3fde0e2e266bed07f1de
Saad_Khan <h.saadkhan501@gmail.com>
2022-08-26 22:18:37 +0500

    movie controller updated

--- VidlyBySaad/Controllers/MovieController.cs
+++ VidlyBySaad/Controllers/MovieController.cs
@@ -15,5 +15,17 @@ namespace VidlyBySaad.Controllers
             var movie = new Movies() { Name = "Shrek" };
             return View(movie);
         }
+
+        public ActionResult Edit(int id)
+        {
+            return Content("id=" + id);
+        }
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+           if(!pageIndex.HasValue)
+            { pageIndex = 1; }
+            if (string.IsNullOrEmpty(sortBy)) { sortBy = "name"; }
+            return Content(string.Format("pageIndex = "+pageIndex+" sort by="+sortBy));
+        }
     }
 }
