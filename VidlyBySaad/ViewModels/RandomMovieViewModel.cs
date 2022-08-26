9ac2412d129a07796cfad5cd1dc7d1afb2fef3dc
Saad_Khan <h.saadkhan501@gmail.com>
2022-08-26 23:50:15 +0500

    view models added

---
+++ VidlyBySaad/ViewModels/RandomMovieViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VidlyBySaad.Models;
+
+namespace VidlyBySaad.ViewModels
+{
+    public class RandomMovieViewModel
+    {
+        public Movies Movie { get; set; }
+        public List<Customer> Customers { get; set; }
+    }
+}
